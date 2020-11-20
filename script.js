c1ccb22814c6b9bbe29aaf2afd09445a5343c775
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-20 22:18:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const money = 1000;
+let income = '';
+const addExpenses = '';
+deposit = 100000;
+mission = 'Accumulate a deposit';
+period = '12';
+
+alert(money);
+console.log(`${mission} ${deposit}$ for ${period} month`);
