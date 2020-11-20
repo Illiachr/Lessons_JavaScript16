9f0d630fa0794f773241d667f02e0d3bf43412a3
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-20 23:11:34 +0200

    lesson 2 task

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const money = 1000;
-let income = '';
-const addExpenses = '';
-const deposit = 100000;
-const mission = 'Accumulate a deposit';
+const money = 30000;
+const income = 'Фриланс';
+const addExpenses = 'Интернет, Мобильный, Коммуналка';
+const deposit = false;
+const mission = 10000;
 const period = '12';
+const budgetDay = money / 30;
 
-alert(money);
-console.log(`${mission} ${deposit}$ for ${period} month`);
+addExpensesLow = addExpenses.toLowerCase().split(',');
+
+console.log(
+    `variable 'money' type is ${typeof money},
+variable 'income' type is ${typeof income},
+string 'addExpenses' lenght is ${addExpenses.length}.
+
+Период равен ${period} месяцев
+Цель заработать ${mission} долларов США
+
+Дополнительные расходы
+    ${addExpenses.toLowerCase().split(',')}
+Заработок в день (budgetDay) ${budgetDay} гривен
+    `    
+);
+
+//    - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+//    - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+
+//    - Вывести в консоль budgetDa
