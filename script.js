e11e1c3f3317c112f9759646938c535766b355ed
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-21 23:01:56 +0200

    изменил обьявление переменных, добавил вывод типа deposit

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const money = 30000;
-const income = 'Фриланс';
-const addExpenses = 'Интернет, Мобильный, Коммуналка';
-const deposit = false;
-const mission = 10000;
-const period = '12';
-const budgetDay = money / 30;
+let money = 30000,
+    income = 'Фриланс',
+    addExpenses = 'Интернет, Мобильный, Коммуналка',
+    deposit = false,
+    mission = 10000,
+    period = '12',
+    budgetDay = money / 30;
 
-addExpensesLow = addExpenses.toLowerCase().split(',');
+const arrExpensesLow = addExpenses.toLowerCase().split(',');
 
 console.log(
     `variable 'money' type is ${typeof money},
 variable 'income' type is ${typeof income},
+variable 'income' type is ${typeof deposit},
 string 'addExpenses' lenght is ${addExpenses.length}.
 
 Период равен ${period} месяцев
 Цель заработать ${mission} долларов США
 
-Дополнительные расходы
-    ${addExpenses.toLowerCase().split(',')}
-Заработок в день (budgetDay) ${budgetDay} гривен
-    `    
+Дополнительные расходы:
+${arrExpensesLow}
+
+budgetDay ${budgetDay} гривен`    
 );
+
+console.log (arrExpensesLow);
