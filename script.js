0a9cf419eca693f01549fa0c75314b209746133d
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-23 14:37:03 +0200

    lesson 3 base task ready

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
 'use strict';
-
-// 9) Написать конструкцию условий (расчеты приведены в рублях)
-// Если budgetDay больше 1200, то “У вас высокий уровень дохода”
-// Если budgetDay больше 600 и меньше 1200, то сообщение “У вас средний уровень дохода”
-// Если budgetDay меньше 600 и больше 0 то в консоль вывести сообщение “К сожалению у вас уровень дохода ниже среднего”
-// Если отрицательное значение то вывести “Что то пошло не так”
-// Учесть варианты 0, 600 и 1200 (к какому уровню не важно)
 // 10) Проверить, чтобы все работало и не было ошибок в консоли
 // 11) Добавить папку с третьим уроком в свой репозиторий на GitHub
 
-let money = +prompt ('Ваш месячный доход?',30000),
+let money = 0,
     income = 'Фриланс',
-    addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Мобильный, Коммуналка'),
-    deposit = confirm ('Есть ли у вас депозит в банке?', false),
+    addExpenses = '',
+    deposit = true,
     mission = 100000,
     period = '12',
-    expenses1 = prompt ('Введите обязательную статью расходов', 'Нет обязательных расходов'),
-    amount1 = +prompt ('Во сколько это обойдется', 0),
-    expenses2 = prompt ('Введите обязательную статью расходов', 'Нет обязательных расходов'),
-    amount2 = +prompt ('Во сколько это обойдется', 0),
-    budgetMonth = money - amount1 - amount2,
-    budgetDay = budgetMonth / 30;
-    
-const re = /\s*,\s*/; //регулярное выражение, чтобы выделить слова без пробелов вокруг, разделитель запятая
+    expenses1 = '',
+    amount1 = 0,
+    expenses2 = '',
+    amount2 = 0,
+    budgetMonth = 0,
+    budgetDay = 0;
 
+money = +prompt ('Ваш месячный доход?',30000);
+if (isNaN(money)) {
+    money = 0;
+} else {
+    addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Мобильный, Коммуналка');
+    deposit = confirm ('Есть ли у вас депозит в банке?', false);
+
+    expenses1 = prompt ('Введите обязательную статью расходов', 'Нет обязательных расходов');        
+    amount1 = +prompt ('Во сколько это обойдется', 0);            
+    if (isNaN(amount1)) amount1 = 0;
+
+    expenses2 = prompt ('Введите обязательную статью расходов', 'Других обязательных расходов нет');
+    amount2 = +prompt ('Во сколько это обойдется', 0);
+    if (isNaN(amount2)) amount2 = 0;
+}
+
+const re = /\s*,\s*/; //регулярное выражение, чтобы выделить слова без пробелов вокруг, разделитель запятая
 const arrExpensesLow = addExpenses.toLowerCase().split(re);
 
-switch (budgetDay) {
-    case budgetDay >= 1200:
-        alert('У Вас высокий уровень дохода');
-        console.log('У Вас высокий уровень дохода');
-        break;
-    case budgetDay > 600 && budgetDay < 1200:
-        alert('У Вас средний уровень дохода');
-        console.log('У Вас средний уровень дохода');
-        break;
-    case budgetDay <= 600:
-        alert('К сожалению у Вас уровень дохода ниже среднего');
-        console.log('К сожалению у Вас уровень дохода ниже среднего');
-        break;
-    case budgetDay <= 0 || budgetDay === NaN:
-        alert('Что-то пошло не так');
-        console.log('Что-то пошло не так');
-        break;
-}
+budgetMonth = money - amount1 - amount2;
+budgetDay = Math.floor(budgetMonth / 30);
+
+if (budgetDay >= 1200) alert('У Вас высокий уровень дохода');
+    else if (budgetDay > 600 && budgetDay < 1200) alert('У Вас средний уровень дохода');
+        else if (budgetDay > 0 && budgetDay <= 600) alert('К сожалению у Вас уровень дохода ниже среднего');
+            else if (budgetDay <= 0) alert('Что-то пошло не так!');
 
 console.log(
     `variable 'money' type is ${typeof money},
@@ -55,10 +52,19 @@ string 'addExpenses' lenght is ${addExpenses.length}.
 Цель заработать ${mission} гривен
 
 bugetMonth = ${budgetMonth} гривен
-budgetDay = ${Math.floor(budgetDay)} гривен
-
-Цель будет достигнута за ${Math.ceil(mission/budgetMonth)} месяцев
+budgetDay = ${budgetDay} гривен
 
 `,
 arrExpensesLow  
 );
+
+let richTarget = 0;
+
+if (budgetMonth > 0) {
+    richTarget = Math.ceil(mission/budgetMonth);
+    if (richTarget === 1) {
+        console.log(`Цель будет достигнута за ${richTarget} месяц`);
+    } else if (richTarget > 1 && richTarget <= 4) {
+        console.log(`Цель будет достигнута за ${richTarget} месяца`);
+    } else console.log(`Цель будет достигнута за ${richTarget} месяцев`);
+} else console.log ('Если Вы не будете зарабатывать, то не сможете достичь цели');
