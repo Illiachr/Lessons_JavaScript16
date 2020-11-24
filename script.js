eebcab882c804f0384c3f91043ece7a3b3f96dc3
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-24 12:28:54 +0200

    добавил функции, регулярку заменил на trim()

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let money = 0,
     amount1 = 0,
     expenses2 = '',
     amount2 = 0,
-//    budgetMonth = 0,
     budgetDay = 0;
 
 money = +prompt ('Ваш месячный доход?',30000);
@@ -34,11 +33,11 @@ const showTypeOf = function(data) {
     return `${data}: ${typeof(data)}`;
 };
 
-const re = /\s*,\s*/; //Выражение для того чтобы взять слова из строка без пробелов вокруг.
 addExpenses = addExpenses.toLowerCase().split(',');
 for (let i = 0; i < addExpenses.length; i++) {
     addExpenses[i] = addExpenses[i].trim();
 }
+
 const getExpensesMonth = function (amntExpn1, amntExpn2) {
     return amntExpn1 + amntExpn2;
 };
@@ -59,23 +58,6 @@ const getTargetMonth = function(target, accM){
         } else { return 'Если Вы не будете зарабатывать, то не сможете достичь цели'; }
 };
 
-// - вызовы функции showTypeOf
-
-//  - Расходы за месяц вызов getExpensesMonth
-
-//  - Вывод возможных расходов в виде массива (addExpenses)
-
-//  - Cрок достижения цели в месяцах (результат вызова функции getTargetMonth) 
-
-//  - Бюджет на день (budgetDay)
-
-//  - вызов функции getStatusIncome
-
-
-// if (budgetDay >= 1200) {console.log('У Вас высокий уровень дохода');}
-//     else if (budgetDay > 600 && budgetDay < 1200) {console.log('У Вас средний уровень дохода');}
-//         else if (budgetDay > 0 && budgetDay <= 600) {console.log('К сожалению у Вас уровень дохода ниже среднего');}
-//             else if (budgetDay <= 0 || budgetDay !== 'number') {console.log('Что-то пошло не так!');}
 const getStatusIncome = ((bD) => {
     return bD >= 1200 ? 'У Вас высокий уровень дохода' :
         bD > 600 && bD < 1200 ? 'У Вас средний уровень дохода' :
@@ -90,13 +72,3 @@ console.log (addExpenses);
 console.log(`Бюджет на день: ${budgetDay} гривен`);
 console.log (getStatusIncome(budgetDay));
 console.log (getTargetMonth(mission, accumulatedMonth));
-
-if (accumulatedMonth > 0) {
-    let richTarget = getTargetMonth(mission,accumulatedMonth);
-    if (richTarget === 1) {
-        console.log(`Цель будет достигнута за ${richTarget} месяц`);
-    } else if (richTarget > 1 && richTarget <= 4) {
-        console.log(`Цель будет достигнута за ${richTarget} месяца`);
-    } else {console.log(`Цель будет достигнута за ${richTarget} месяцев`);}
-} else {console.log ('Если Вы не будете зарабатывать, то не сможете достичь цели');}
-
