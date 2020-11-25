4e74ea9a14741cb0ea832b85413ce1151ce6e6a3
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-25 15:42:57 +0200

    добавил фукцию start и проверку чисел

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
 'use strict';
 
-let money = 0,
+let money,
     income = 'Фриланс',
     addExpenses = '',
     deposit = true,
     mission = 100000,
     period = '12',
-    expenses1 = '',
-    amount1 = 0,
-    expenses2 = '',
-    amount2 = 0,
+    amountSum = 0,  
     budgetDay = 0;
 
-money = +prompt ('Ваш месячный доход?',30000);
-if (isNaN(money)) {
-    money = 0;
-} else {
-    addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 
-    'Интернет, Мобильный, Коммуналка');
-    deposit = confirm ('Есть ли у вас депозит в банке?', false);
-
-    expenses1 = prompt ('Введите обязательную статью расходов', 'Нет обязательных расходов');        
-    amount1 = +prompt ('Во сколько это обойдется', 0);            
-    if (isNaN(amount1)) {amount1 = 0;}
-
-    expenses2 = prompt ('Введите обязательную статью расходов', 'Других обязательных расходов нет');
-    amount2 = +prompt ('Во сколько это обойдется', 0);
-    if (isNaN(amount2)) {amount2 = 0;}
-}
+const userExpenses = {
+        expenses: [],
+        amount: []
+    };
 
 const showTypeOf = function(data) {
     return `${data}: ${typeof(data)}`;
 };
+
+const isNumber = ((x) => {
+    return !isNaN(parseFloat(x)) && isFinite(parseFloat(x));
+});
+
+const start = (() => {
+    do {
+        money = prompt('Ваш месячный доход?','30000');
+    } while (!isNumber(money));
+    money = parseFloat(money);
+});
+
+start();
+
+addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 
+'Интернет, Мобильный, Коммуналка');
+deposit = confirm ('Есть ли у вас депозит в банке?', false);
 
 addExpenses = addExpenses.toLowerCase().split(',');
 for (let i = 0; i < addExpenses.length; i++) {
     addExpenses[i] = addExpenses[i].trim();
 }
 
-const getExpensesMonth = function (amntExpn1, amntExpn2) {
-    return amntExpn1 + amntExpn2;
-};
+const getExpensesMonth = (() => {
+    let sum = 0;
+    for (let i = 0; i < 3; i++) {
+        userExpenses.expenses[i] = prompt ('Введите обязательную статью расходов', ''); 
+        do {
+            userExpenses.amount[i] = prompt ('Во сколько это обойдется', '0');        
+        } while (!isNumber(userExpenses.amount[i]));
+        
+        userExpenses.amount[i] = parseFloat(userExpenses.amount[i]);
+        sum = sum + userExpenses.amount[i];
+    } 
+    return sum;    
+});
 
-const getAccumulatedMonth = function (totalIncome, amntExpn1, amntExpn2, callback){
-    return money - getExpensesMonth(amntExpn1, amntExpn2);
-};
+amountSum = getExpensesMonth();
+const getAccumulatedMonth = ((sum) => {
+    return money - sum;
+});
 
-let accumulatedMonth = getAccumulatedMonth (money, amount1, amount2, getExpensesMonth);
+let accumulatedMonth = getAccumulatedMonth(amountSum);
 budgetDay = Math.floor(accumulatedMonth/ 30);
 
-const getTargetMonth = function(target, accM){    
+const getTargetMonth = ((target, accM) => {    
     if (accM > 0) {
         let richTarget = Math.ceil(target/accM);
         return richTarget === 1 ? `Цель будет достигнута за ${richTarget} месяц` :
         richTarget > 1 && richTarget <= 4 ? `Цель будет достигнута за ${richTarget} месяца` : 
         `Цель будет достигнута за ${richTarget} месяцев`;
         } else { return 'Если Вы не будете зарабатывать, то не сможете достичь цели'; }
-};
+});
 
-const getStatusIncome = ((bD) => {
-    return bD >= 1200 ? 'У Вас высокий уровень дохода' :
-        bD > 600 && bD < 1200 ? 'У Вас средний уровень дохода' :
-        bD > 0 && bD <= 600 ? 'К сожалению у Вас уровень дохода ниже среднего' : 'Что-то пошло не так!';
+const getStatusIncome = ((bd) => {
+    return bd >= 1200 ? 'У Вас высокий уровень дохода' :
+        bd > 600 && bd < 1200 ? 'У Вас средний уровень дохода' :
+        bd > 0 && bd <= 600 ? 'К сожалению у Вас уровень дохода ниже среднего' : 'Что-то пошло не так!';
 });
 
 console.log (showTypeOf(money));
 console.log (showTypeOf(income));
 console.log (showTypeOf(deposit));
-console.log (getExpensesMonth(amount1, amount2));
+console.log (`Расходы за месяц ${amountSum}`);
 console.log (addExpenses);
 console.log(`Бюджет на день: ${budgetDay} гривен`);
 console.log (getStatusIncome(budgetDay));
 console.log (getTargetMonth(mission, accumulatedMonth));
+console.log (userExpenses);
