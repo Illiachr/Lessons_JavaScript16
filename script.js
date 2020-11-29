548ff1990bc9ff61fd7c625c2c6987e091398370
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-29 12:44:01 +0200

    добавил this, скорректировал вывод данных

--- script.js
+++ script.js
@@ -27,55 +27,76 @@ let appData = {
         budgetMonth     : 0,
         expensesMonth   : 0,
         richTarget      : 0,
-        asking          : (() => {
-            appData.addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 
-            'Интернет, Мобильный, Коммуналка').toLowerCase().split(',');
-            appData.deposit = confirm ('Есть ли у вас депозит в банке?', false);
+        asking () {
+            this.addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую', 
+            'Интернет, Мобильный, Коммуналка')
+            .toLowerCase().split(',')
+            .map(item => item = item.trim());
+            this.deposit = confirm ('Есть ли у вас депозит в банке?', false);
 
             let i = 0;
             let key = '';
             for (i ; i < 4; i++) {
                 key = prompt ('Введите обязательную статью расходов');
                 do {
-                appData.Expenses[key] = prompt ('Во сколько это обойдется', '0');
-                } while (!isNumber(appData.Expenses[key]));
+                    this.Expenses[key] = prompt ('Во сколько это обойдется', '0');
+                } while (!isNumber(this.Expenses[key]));
             }
-        }),
+        },
 
-        getExpensesMonth : (() => {
-            for (let key in appData.Expenses) {                    
-                appData.expensesMonth = appData.expensesMonth + parseFloat(appData.Expenses[key]);
+        getExpensesMonth () {
+            for (let key in this.Expenses) {                    
+                this.expensesMonth = this.expensesMonth + parseFloat(appData.Expenses[key]);
             }  
-        }),
+        },
         
-        getBudget : (() => {
-            appData.budgetMonth = appData.budget - appData.expensesMonth;
-            appData.budgetDay = Math.floor(appData.budgetMonth/30);
-        }),
+        getBudget () {
+            this.budgetMonth = this.budget - this.expensesMonth;
+            this.budgetDay = Math.floor(appData.budgetMonth/30);
+        },
         
-        getTargetMonth      : (() => {                
-                appData.richTarget = Math.ceil(appData.mission/appData.budgetMonth);
-        }),
+        getTargetMonth () {                
+            this.richTarget = Math.ceil(this.mission/this.budgetMonth);
+            if (this.richTarget > 0)    {
+                console.log (`Цель будет достигнута за ${this.richTarget} месяца`);
+            } else {
+                console.log(`Цель не достижима! Необходимо получать доход`);
+            }
+        },
         
-        getStatusIncome : (() => {
+        getStatusIncome () {
             return appData.budgetDay >= 1200 ? 'У Вас высокий уровень дохода' :
                 appData.budgetDay > 600 && appData.budgetDay < 1200 ? 'У Вас средний уровень дохода' :
                 appData.budgetDay > 0 && appData.budgetDay <= 600 ? 
                 'К сожалению у Вас уровень дохода ниже среднего' : 'Что-то пошло не так!';
-        })        
+        }        
     };
 
 appData.budget = parseFloat(money);
 appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
-appData.getTargetMonth();
+
 
 console.log (`Расходы за месяц: ${appData.expensesMonth} гривен`);
-console.log (`Цель будет достигнута за ${appData.richTarget} месяцев`);
+appData.getTargetMonth();
+//console.log (`Цель будет достигнута за ${appData.richTarget} месяцев`);
 console.log(appData.getStatusIncome());
 
-console.log (`Наша программа включает в себя следующие данные:`);
+console.log (`
+------------------------------------------------
+Наша программа включает в себя следующие данные:
+------------------------------------------------
+`);
 for (let key in appData) {
-    console.log(`${key} : ${appData[key]}`);
+    if (appData.hasOwnProperty(key))
+    {
+        if (key === 'income' || key === 'Expenses') {
+            console.log(`${key} : `);
+            console.log(appData[key]);
+            
+        } else{
+            console.log(`${key} : ${appData[key]}`);
+        }
+    }
 }
